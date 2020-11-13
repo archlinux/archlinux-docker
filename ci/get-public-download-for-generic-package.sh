9c39d53bafc3cfecf04620c01071569bcde17e8a
Justin Kromlinger <hashworks@archlinux.org>
2020-11-13 11:07:13 +0100

    Always retrieve the latest package from the API
    Without this stuff fails after a while since the API is paginated and
    sorted by ASC by default.

--- ci/get-public-download-for-generic-package.sh
+++ ci/get-public-download-for-generic-package.sh
@@ -4,6 +4,6 @@ set -eu
 
 package_name=$1
 
-package_id=$(curl -sSf --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_TOKEN}" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages" | jq ".[] | select(.version == \"${BUILD_VERSION}\") | .id")
+package_id=$(curl -sSf --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_TOKEN}" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages?sort=desc&per_page=1" | jq ".[] | select(.version == \"${BUILD_VERSION}\") | .id")
 package_file_id=$(curl -sSf --header "PRIVATE-TOKEN: ${GITLAB_PROJECT_TOKEN}" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/${package_id}/package_files" | jq ".[] | select(.file_name == \"$package_name\") | .id")
 echo "https://gitlab.archlinux.org/archlinux/archlinux-docker/-/package_files/${package_file_id}/download"
