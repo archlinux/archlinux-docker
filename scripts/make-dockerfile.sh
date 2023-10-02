d1254eeee40214ab5de307e80e4c705c4603b883
Emil Velikov <emil.l.velikov@gmail.com>
2023-10-02 12:46:17 +0100

    scripts/make-dockerfile.sh: reuse in the release stage
    Bonus point, we actually error out when the git command fails.
    
    v2:
     - fix curl quoting
     - sed match-complete-line-and-remove
     - inline update make-dockerfile.sh variables
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- scripts/make-dockerfile.sh
+++ scripts/make-dockerfile.sh
@@ -4,12 +4,18 @@ set -euo pipefail
 
 declare -r GROUP="$1"
 declare -r OUTPUTDIR="$2"
+declare -r DOWNLOAD="$3"
+declare -r TITLE="$4"
+
+# Do not use these directly in the sed below - it will mask git failures
+BUILD_VERSION="${BUILD_VERSION:-dev}"
+CI_COMMIT_SHA="${CI_COMMIT_SHA:-$(git rev-parse HEAD)}"
 
 sed -e "s|TEMPLATE_ROOTFS_FILE|$GROUP.tar.zst|" \
-    -e "s|TEMPLATE_ROOTFS_DOWNLOAD|true|" \
+    -e "s|TEMPLATE_ROOTFS_DOWNLOAD|$DOWNLOAD|" \
     -e "s|TEMPLATE_ROOTFS_HASH|$(cat $OUTPUTDIR/$GROUP.tar.zst.SHA256)|" \
-    -e "s|TEMPLATE_TITLE|Arch Linux Dev Image|" \
-    -e "s|TEMPLATE_VERSION_ID|dev|" \
-    -e "s|TEMPLATE_REVISION|$(git rev-parse HEAD)|" \
+    -e "s|TEMPLATE_TITLE|Arch Linux $TITLE Image|" \
+    -e "s|TEMPLATE_VERSION_ID|$BUILD_VERSION|" \
+    -e "s|TEMPLATE_REVISION|$CI_COMMIT_SHA|" \
     -e "s|TEMPLATE_CREATED|$(date -Is)|" \
     Dockerfile.template > "$OUTPUTDIR/Dockerfile.$GROUP"
