4b15f9a1a11d11474cad201521962dc6192630ea
Robin Candau <antiz@archlinux.org>
2026-04-02 09:24:16 +0200

    Remove ldconfig auxiliary cache file only for the repro group

--- scripts/make-dockerfile.sh
+++ scripts/make-dockerfile.sh
@@ -13,11 +13,13 @@ declare -rx SOURCE_DATE_EPOCH="$6"
 BUILD_VERSION="${BUILD_VERSION:-dev}"
 CI_COMMIT_SHA="${CI_COMMIT_SHA:-$(git rev-parse HEAD)}"
 
-# Honor SOURCE_DATE_EPOCH for the repro GROUP
+# Honor SOURCE_DATE_EPOCH and delete non-determistic ldconfig auxiliary cache file for the repro GROUP
 if [[ "$GROUP" == "repro" ]]; then
     CREATED_TIMESTAMP=$(date -u -d "@$SOURCE_DATE_EPOCH" +%Y-%m-%dT%H:%M:%SZ)
+    LDCONFIG_AUX_CACHE="rm -f /var/cache/ldconfig/aux-cache"
 else
     CREATED_TIMESTAMP=$(date -Is)
+    LDCONFIG_AUX_CACHE="true"
 fi
 
 sed -e "s|TEMPLATE_ROOTFS_FILE|$ROOTFS_FILE|" \
@@ -27,4 +29,5 @@ sed -e "s|TEMPLATE_ROOTFS_FILE|$ROOTFS_FILE|" \
     -e "s|TEMPLATE_VERSION_ID|$BUILD_VERSION|" \
     -e "s|TEMPLATE_REVISION|$CI_COMMIT_SHA|" \
     -e "s|TEMPLATE_CREATED|$CREATED_TIMESTAMP|" \
+    -e "s|LDCONFIG_AUX_CACHE|$LDCONFIG_AUX_CACHE|" \
     Dockerfile.template > "$OUTPUTDIR/Dockerfile.$GROUP"
